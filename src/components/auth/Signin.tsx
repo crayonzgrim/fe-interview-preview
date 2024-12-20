390970d43fcf3d9a9bb217059ec0c172e9c935a8
KIM DONG IL <dongil.kim@anti-agingclub.kr>
2024-12-20 09:37:42 +0900

    :lipstick: Add shadcn/ui

--- src/components/auth/Signin.tsx
+++ src/components/auth/Signin.tsx
@@ -1,3 +1,18 @@
+import { signinSchema } from "@/components/auth";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
 export const Signin = () => {
+  /** Property */
+  const form = useForm<z.infer<typeof signinSchema>>({
+    resolver: zodResolver(signinSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  /** Render */
   return <div></div>;
 };
