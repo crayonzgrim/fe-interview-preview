6d93a2db666f508dbac5aa776678e063b12a3d9d
KIM DONG IL <dongil.kim@anti-agingclub.kr>
2024-12-20 09:30:32 +0900

    :hammer: Signup form schema

--- src/components/auth/schema.ts
+++ src/components/auth/schema.ts
@@ -8,3 +8,15 @@ export const signinSchema = z.object({
     message: "최소 3자 이상 작성해주세요",
   }),
 });
+
+export const signupSchema = z.object({
+  name: z.string().min(1, {
+    message: "이름은 필수입니다!",
+  }),
+  email: z.string().email({
+    message: "올바른 이메일을 입력해주세요",
+  }),
+  password: z.string().min(3, {
+    message: "최소 3자 이상 작성해주세요",
+  }),
+});
